baaafc5fddc71ed25dbb3945b83042ab5e5ffd7a
devnahin <105677983+devnahin@users.noreply.github.com>
2023-02-06 19:17:24 +0600

    Ostad Assignment
    Tuition Fees Commission Calculation

---
+++ tution_fees_calculation.php
@@ -0,0 +1,19 @@
+<?php
+
+$tutionFees = 9000;
+
+
+//commission 25% for tution fees above 20,000USD 
+$commission_25 = $tutionFees * .25; 
+
+//commission 20% for tution fees above 10,000USD below 20,000USD 
+$commission_20 = $tutionFees * .20; 
+
+//commission 15% for tution fees above 7,000USD below 10,000USD 
+$commission_15 = $tutionFees * .15; 
+
+
+//ternary operator to calculate this calculation 
+
+print ($tutionFees >= 20000) ? 'Commission 25% = '. $commission_25  : (( $tutionFees >= 10000 && $tutionFees <= 20000) ? 'Commission 20% = '. $commission_20 : (($tutionFees >= 7000 && $tutionFees <= 10000) ? 'Commission 15% = '. $commission_15 : 'Invalid') );
+
